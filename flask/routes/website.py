1c8b72c56e63aa0c8880a72adbdd9a80eed234e1
tayler6000 <taylerporter@gmail.com>
2022-08-01 14:33:28 -0500

    Changing template function on flask server

--- flask/routes/website.py
+++ flask/routes/website.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template_string
+import os
 
+
+def render_template(file, **context):
+    with open(os.path.join('flask', 'templates', file), 'r') as f:
+        return render_template_string(f.read(), **context)
 
 def index():
     return render_template("index.html")
