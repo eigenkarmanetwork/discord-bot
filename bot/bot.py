1c73461735580fa1fde3eea2ce4727d2d7d703cc
tayler6000 <taylerporter@gmail.com>
2022-07-31 23:41:53 -0500

    Added better argument handling.  Args could now be passed into argsparser if desired. Added basic help function.

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 import dotenv
 import os
+import shlex
 import threading
 
 dotenv.load_dotenv()
@@ -21,12 +22,28 @@ class DiscordHandler:
 
         @self.client.event
         async def on_message(message: discord.message.Message) -> None:
-            print("Discord bot received a message!")
             if message.clean_content[0:5].lower() == "!etn ":
-                command = message.clean_content[5:]
+                raw_command = shlex.split(message.clean_content[5:])
+                command = raw_command[0]
+                args = raw_command[1:]
                 print(f"Received Command: `{command}`")
                 if command.lower() == "ping":
                     await message.channel.send("Pong!")
+                elif command.lower() == "help":
+                    if len(args) == 1:
+                        match args[0]:
+                            case "ping":
+                                await message.channel.send("Makes the ETN bot say `Pong!` as soon as possible.")
+                            case "help":
+                                await message.channel.send("Attempts to give help for any given command.")
+                            case _:
+                                await message.channel.send(f"No help for: `{args[0]}`")
+                    elif len(args) == 0:
+                        await message.channel.send("Commands:\n\nping\nhelp")
+                    else:
+                        await message.channel.send("Cannot provide help for multiple commands at once.")
+                else:
+                    await message.channel.send(f"Unknown Command: `{command}`")
 
         @self.client.event
         async def on_raw_reaction_add(payload: discord.raw_models.RawReactionActionEvent) -> None:
