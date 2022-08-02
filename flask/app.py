26ec2e3cff0dfc0ed51a6109952f7baab36edabc
tayler6000 <taylerporter@gmail.com>
2022-08-02 13:26:34 -0500

    Allowing post on login

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.add_url_rule("/", view_func=website.index)
 app.add_url_rule("/authorize_discord", view_func=api.authorize_discord)
-app.add_url_rule("/connect", view_func=api.connect)
+app.add_url_rule("/connect", view_func=api.connect, methods=["POST"])
 app.add_url_rule("/connected.html", view_func=website.connected)
 app.add_url_rule("/index.html", view_func=website.index)
 app.add_url_rule("/login", view_func=website.login)
